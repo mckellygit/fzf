f82e8b4698acb5ad51853233132879295e05e958
Mark Kelly <mckelly2833@outlook.com>
2023-05-26 08:38:52 -0400

    mck mods - stable result order, exact+case-insens with double-quote

--- src/result_x86.go
+++ src/result_x86.go
@@ -3,6 +3,8 @@
 package fzf
 
 import "unsafe"
+import "strings"
+// import "fmt"
 
 func compareRanks(irank Result, jrank Result, tac bool) bool {
 	left := *(*uint64)(unsafe.Pointer(&irank.points[0]))
@@ -11,6 +13,30 @@ func compareRanks(irank Result, jrank Result, tac bool) bool {
 		return true
 	} else if left > right {
 		return false
-	}
-	return (irank.item.Index() <= jrank.item.Index()) != tac
+    }
+
+    // fmt.Print("i,j = ", irank.item.Index(), " ", irank.item.text.ToString(), " ", jrank.item.Index(), " ", jrank.item.text.ToString())
+    // fmt.Println()
+
+    var cval = strings.Compare(irank.item.text.ToString(), jrank.item.text.ToString())
+
+    if (!tac) {
+
+        if cval <= 0 {
+            return true
+        } else {
+            return false
+        }
+
+    } else {
+
+        if cval <= 0 {
+            return false
+        } else {
+            return true
+        }
+
+    }
+
+	// return (irank.item.Index() <= jrank.item.Index()) != tac
 }
